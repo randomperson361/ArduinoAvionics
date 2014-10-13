dc2a5d7dc503f23e3afe51e4225d7a7776dcd3dc
Andrew McNeill <mcneill.andrew.d@gmail.com>
2014-10-13 02:19:53 -0500

    added pin definitions

--- ArduinoAvionics.ino
+++ ArduinoAvionics.ino
@@ -1,13 +1,34 @@
 #include "ds3234.h"
 
+#define RADIO_RX_PIN 	0
+#define RADIO_TX_PIN 	1
+
+#define DECTALK_RX_PIN	2
+#define DECTALK_TX_PIN	3
+
+#define GPS_RX_PIN		7
+#define GPS_TX_PIN		8
+
+#define HUMITIDY_PIN	5
+#define PITOT_PIN		A0
+#define BATT_VOLT_PIN	A1
+
+#define DS3234_SS_PIN 	10
+#define SD_SS_PIN 		4
+#define MOSI_PIN 		11
+#define MISO_PIN 		12
+#define SPI_CLK_PIN 	13
+
+
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+
 }
