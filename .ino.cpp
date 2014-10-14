180aa608b161a084814f1e46b6246ba34b27a686
Andrew McNeill <mcneill.andrew.d@gmail.com>
2014-10-13 22:53:25 -0500

    added IMU and EMIC libraries

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2014-10-13 21:57:49
+//This file has been generated on 2014-10-13 22:52:23
 
 #include "Arduino.h"
 #include <Arduino.h>
