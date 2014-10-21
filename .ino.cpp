f3af51896d3fbb632bdf79bc83ce2746a239f5a6
Andrew McNeill <mcneill.andrew.d@gmail.com>
2014-10-21 11:45:04 -0500

    GPS testing

--- .ino.cpp
+++ .ino.cpp
@@ -1,29 +1,15 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2014-10-21 11:15:38
+//This file has been generated on 2014-10-21 11:37:05
 
 #include "Arduino.h"
-#include <Arduino.h>
-#include <EEPROM.h>
-#include <SD.h>
+#include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
-#include <Wire.h>
-#include "Adafruit_Sensor.h"
-#include "Adafruit_LSM303_U.h"
-#include "Adafruit_BMP085_U.h"
-#include "Adafruit_L3GD20_U.h"
-#include "Adafruit_10DOF.h"
-#include "DHT.h"
-#include "DHT_U.h"
-#include "Adafruit_GPS.h"
-static uint8_t openLogFile() ;
-static void closeLogFile() ;
-static void printTime() ;
-static void processRadio(uint8_t Signal) ;
-static void checkRadioCommands() ;
 void setup() ;
-void loop() ;
+SIGNAL(TIMER0_COMPA_vect) ;
+void useInterrupt(boolean v) ;
+void loop()                      ;
 
 
 #include "ArduinoAvionics.ino"
