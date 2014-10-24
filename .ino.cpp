f6d965c8f96e4d53997629bfa4c20ab6ca133049
Andrew McNeill <mcneill.andrew.d@gmail.com>
2014-10-24 10:37:30 -0500

    added gyro and dht to testing, all working together

--- .ino.cpp
+++ .ino.cpp
@@ -1,14 +1,17 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2014-10-24 10:10:20
+//This file has been generated on 2014-10-24 10:35:41
 
 #include "Arduino.h"
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
 #include <Adafruit_10DOF.h>
+#include <DHT.h>
+#include <DHT_U.h>
 void initSensors() ;
 void setup(void) ;
 void loop(void) ;
--- ArduinoAvionics.ino
+++ ArduinoAvionics.ino
@@ -2,13 +2,28 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
 #include <Adafruit_10DOF.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN			  5
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
 
 /* Assign a unique ID to the sensors */
 Adafruit_10DOF                dof   = Adafruit_10DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+Adafruit_L3GD20_Unified 	  gyro = Adafruit_L3GD20_Unified(20);
 Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
+DHT_Unified 				  dht(DHTPIN, DHTTYPE);
+sensors_event_t accel_event;
+sensors_event_t mag_event;
+sensors_event_t bmp_event;
+sensors_vec_t   orientation;
+sensors_event_t dht_event;
+sensors_event_t gyro_event;
+
 
 /* Update this with the correct SLP for accurate altitude measurements */
 float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
@@ -38,6 +53,19 @@ void initSensors()
     Serial.println("Ooops, no BMP180 detected ... Check your wiring!");
     while(1);
   }
+
+  /* Enable auto-ranging */
+  gyro.enableAutoRange(true);
+
+  /* Initialise the sensor */
+  if(!gyro.begin())
+  {
+    /* There was a problem detecting the L3GD20 ... check your connections */
+    Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
+    while(1);
+  }
+
+  dht.begin();
 }
 
 /**************************************************************************/
@@ -61,11 +89,6 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
-  sensors_event_t accel_event;
-  sensors_event_t mag_event;
-  sensors_event_t bmp_event;
-  sensors_vec_t   orientation;
-
   /* Read the accelerometer and magnetometer */
   accel.getEvent(&accel_event);
   mag.getEvent(&mag_event);
@@ -82,28 +105,6 @@ void loop(void)
     Serial.print(orientation.heading);
     Serial.println(F(""));
   }
-
-  /* Previous code removed handling accel and mag data separately */
-  //  /* Calculate pitch and roll from the raw accelerometer data */
-  //  Serial.print(F("Orientation: "));
-  //  accel.getEvent(&accel_event);
-  //  if (dof.accelGetOrientation(&accel_event, &orientation))
-  //  {
-  //    /* 'orientation' should have valid .roll and .pitch fields */
-  //    Serial.print(orientation.roll);
-  //    Serial.print(F(" "));
-  //    Serial.print(orientation.pitch);
-  //    Serial.print(F(" "));
-  //  }
-  //
-  //  /* Calculate the heading using the magnetometer */
-  //  mag.getEvent(&mag_event);
-  //  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
-  //  {
-  //    /* 'orientation' should have valid .heading data now */
-  //    Serial.print(orientation.heading);
-  //  }
-  //  Serial.println(F(""));
 
   /* Calculate the altitude using the barometric pressure sensor */
   bmp.getEvent(&bmp_event);
@@ -122,7 +123,39 @@ void loop(void)
     Serial.print(F("Temp: "));
     Serial.print(temperature);
     Serial.println(F(""));
+    Serial.print(F("Pressure "));
+    Serial.print(bmp_event.pressure);
+    Serial.println(F(""));
   }
 
-  delay(1000);
+  /* Get a new sensor event */
+  gyro.getEvent(&gyro_event);
+
+  /* Display the results (speed is measured in rad/s) */
+  Serial.print("X: "); Serial.print(gyro_event.gyro.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(gyro_event.gyro.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(gyro_event.gyro.z); Serial.print("  ");
+  Serial.println("rad/s ");
+
+	dht.temperature().getEvent(&dht_event);
+	if (isnan(dht_event.temperature)) {
+	  Serial.println(F("Error reading temperature!"));
+	}
+	else {
+	  Serial.print(F("Temperature: "));
+	  Serial.print(dht_event.temperature);
+	  Serial.println(F(" *C"));
+	}
+	// Get humidity event and print its value.
+	dht.humidity().getEvent(&dht_event);
+	if (isnan(dht_event.relative_humidity)) {
+	  Serial.println(F("Error reading humidity!"));
+	}
+	else {
+	  Serial.print(F("Humidity: "));
+	  Serial.print(dht_event.relative_humidity);
+	  Serial.println(F("%\n\n"));
+	}
+
+  delay(2000);
 }
